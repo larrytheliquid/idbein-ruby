1bc016e1d590daea40106510f96a53d0db10458a
Zachary Berry <zberry@glenda.local>
2009-04-18 14:03:57 -0400

    made the clicking action to a js update to increment the number.  also prevents you from incrementing the number over and over again.

--- public/includes/main.js
+++ public/includes/main.js
@@ -1,6 +1,7 @@
 /**
  * @author zberry
  */
+
 function hilite(element)
 {
 	element.style.backgroundColor = '#eeeeee';
@@ -13,14 +14,19 @@ function unhilite(element)
 
 function handleClick(permalink, username)
 {
+	var voted = $('#cb_i_' + permalink)[0].className == "voted";
+	
+	if(!voted)
+	{
 	var url = '/polls/' + permalink + '/votes/' + username;
-	jQuery.ajax({
-		type: 'put',
-		url: url,
-		success: handleClickSuccess(permalink)}
-		//dataType: type
-	);
-}
+		jQuery.ajax({
+			type: 'put',
+			url: url,
+			success: handleClickSuccess(permalink)}
+			//dataType: type
+		);
+		
+	}
 
 function handleClickSuccess(permalink)
 {
@@ -30,16 +36,21 @@ function handleClickSuccess(permalink)
 	var checkMsg = $("#cb_m_" + permalink)[0];
 	checkMsg.innerHTML = "I'm in!";
 	checkImage.className = 'voted';
+	
+	var voteCount = $("#cb_v_" + permalink)[0];
+	voteCount.innerHTML = Number(voteCount.innerHTML) + 1;
+	
 }
 
 function togglePoll()
 {
 	var f = $('#add-poll-form')[0];
-	f.style.display = f.style.display == "none" || f.style.display == "" ? "block" : "none";
+	f.style.display = (f.style.display == "none" || f.style.display == "" ? "block" : "none");
 }
 
 function submitNewPoll()
 {
+	/*
 	$.ajax({
 		type: "POST",
 		url: "bin/process.php",
@@ -47,5 +58,5 @@ function submitNewPoll()
 		success: function() {
 			alert('success');
 		}
-	});
-}
+	});*/
+}}
