75db5465a43640f965f43f8e0833442d00147c3a
Larry Diehl <larry@laptoptheliquid.gateway.2wire.net>
2009-05-24 20:15:17 -0700

    Combined step definitions into one file

--- test/features/step_definitions/general_steps.rb
+++ test/features/step_definitions/general_steps.rb
@@ -5,3 +5,60 @@ end
 When /^I go to the polls list$/ do
   visit "#{APP}/polls"
 end
+
+When /^I try to add a new poll$/ do
+  click_link 'Add a Poll'
+end
+
+When /^I submit all required poll fields$/ do
+  fill_in 'Title', :with => 'idbein beta invite'
+  fill_in 'Description', :with => 'Want one? vote up while you have the chance!'
+  fill_in 'Threshold', :with => '200'
+  click_button 'Add Poll'
+end
+
+Then /^my poll should show up in the polls list$/ do
+  response.should contain(CGI.escape 'idbein beta invite')
+end
+
+Given /^I am not logged in$/ do
+  # noop
+end
+
+When /^I try to signup$/ do
+  click_link 'Signup'
+end
+
+When /^I submit all required user fields$/ do
+  fill_in 'Username', :with => 'larrytheliquid'
+  fill_in 'Email', :with => 'larrytheliquid@gmail.com'
+  click_button 'Submit'
+end
+
+Then /^I should be redirected to the polls list$/ do
+  response.uri.request_uri.should == '/polls'
+end
+
+Then /^I should be logged in$/ do
+  response.should contain(h 'Hello larrytheliquid!')
+end
+
+Given /^a candidate previously created a poll$/ do
+  new_poll(:title => 'poll', :threshold => 5).save
+end
+
+Given /^I previously voted for the poll$/ do
+  RestClient.put "#{APP}/polls/poll/votes/#{current_user.id}", ''
+end
+
+When /^I vote for the poll$/ do
+  RestClient.put "#{APP}/polls/poll/votes/#{current_user.id}", ''
+end
+
+Then /^the number of votes for it should be incremented$/ do
+  response.body.strip_html.should contain('1/5')
+end
+
+Then /^the number of votes for it should not be incremented$/ do
+  response.body.strip_html.should contain('1/5')
+end
